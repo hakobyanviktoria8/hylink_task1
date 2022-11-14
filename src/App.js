86e9202f59c328d7402a0095f49afe0a4aa5a9b9
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-11-14 16:30:16 +0400

    create project and add it github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [showMore, setShowMore] = useState(false);
+  
+  const handleClick = () => {
+    setShowMore(!showMore)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Username </th>
+            <th>Type</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td><button onClick={handleClick}>btn</button></td>
+          </tr>
+          {showMore && 
+           <tr>
+            <td colspan="4">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Company</th>
+                  <th>Location</th>
+                  <th>Repositories</th>
+                  <th>Followers</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Alfreds Futterkiste</td>
+                  <td>Maria Anders</td>
+                  <td>Germany</td>
+                  <td>btn</td>
+                  <td>wqw</td>
+                </tr>
+              </tbody>
+            </table>
+            </td>
+          </tr>
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
