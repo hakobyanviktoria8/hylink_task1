06a3f0171502c2aa764b55db771d30e9864c07ca
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-11-14 23:41:22 +0400

    added paginatin, use react-paginate npm

--- src/App.js
+++ src/App.js
@@ -2,32 +2,59 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import User from './util/components/User';
 import { fetchData } from './util/helpers/fetchData';
+import ReactPaginate from 'react-paginate';
 
 function App() {
   const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  // if we want can change per page users count 20 
+  const usersPerPage = 10;
+  const [perPageUsersCount, setPerPageUsersCount] = useState(0);
+  const endUsersCount = perPageUsersCount + usersPerPage;
+  const currentUsersData = users?.slice(perPageUsersCount, endUsersCount);
+  const pageCount = Math.ceil(users?.length / usersPerPage);
   
   useEffect(()=> {
     fetchData("https://api.github.com/users", setUsers)
+    setIsLoading(true)
   },[])
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * usersPerPage) % users.length;
+    setPerPageUsersCount(newOffset);
+  };  
   
   return (
-    <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Avatar</th>
-            <th>Username </th>
-            <th>Type</th>
-            <th>Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users?.map(user => 
-            <User user={user}/>
-          )}
-        </tbody>
-      </table>
-    </div>
+    !isLoading ? 
+      <h2>Loading...</h2>
+      : 
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>Avatar</th>
+              <th>Username </th>
+              <th>Type</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentUsersData?.map(user => 
+              <User user={user}/>
+            )}
+          </tbody>
+        </table>
+        
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+        />
+      </div>
   );
 }
 
